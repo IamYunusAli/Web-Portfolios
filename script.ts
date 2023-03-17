3fb429dc7c4bcd732737d64fe3ded5ad0c9bf1a7
iamyunusali <aliyunus291@gmail.com>
2023-03-18 00:31:44 +0300

    change it to typescript

---
+++ script.ts
@@ -0,0 +1,85 @@
+// const headerTag = document.querySelector("header")
+// const blobGroups = document.querySelectorAll("g.blob")
+// const sectionTags = document.querySelectorAll("section")
+// const arrowTag = document.querySelector("img.arrow")
+
+// const easing = function (x) {
+//   return x * x * x
+// }
+
+// const fadeHeader = function () {
+//   const pixels = window.pageYOffset
+
+//   headerTag.style.opacity = 1 - easing(pixels / 500)
+// }
+
+// const fadeArrow = function () {
+//   const pixels = window.pageYOffset
+
+//   arrowTag.style.opacity = 1 - easing(pixels / 250)
+// }
+
+// const checkBlobs = function () {
+//   const pixels = window.pageYOffset
+
+//   blobGroups.forEach((blob, index) => {
+//     const currentSection = sectionTags[index]
+
+//     if (pixels > currentSection.offsetTop - 300) {
+//       blob.classList.add("in-view")
+//     } else {
+//       blob.classList.remove("in-view")
+//     }
+//   })
+// }
+
+
+// window.addEventListener("scroll", function () {
+//   fadeHeader()
+//   fadeArrow()
+//   checkBlobs()
+// })
+
+
+const headerTag = document.querySelector("header")!
+const blobGroups = document.querySelectorAll("g.blob")
+const sectionTags = document.querySelectorAll("section")
+const arrowTag = document.querySelector("img.arrow")! as HTMLElement;
+
+const easing = (x: number) => {
+  return x * x * x
+}
+
+const fadeHeader = () => {
+  const pixels = window.pageYOffset
+
+  headerTag.style.opacity = (1 - easing(pixels / 500)).toString()
+}
+
+const fadeArrow = () => {
+  const pixels = window.pageYOffset
+
+  arrowTag.style.opacity = (1 - easing(pixels / 250)).toString()
+}
+
+const checkBlobs = () => {
+  const pixels = window.pageYOffset
+
+  blobGroups.forEach((blob, index) => {
+    const currentSection = sectionTags[index]
+
+    if (pixels > currentSection.offsetTop - 300) {
+      blob.classList.add("in-view")
+    } else {
+      blob.classList.remove("in-view")
+    }
+  })
+}
+
+
+window.addEventListener("scroll", () => {
+  fadeHeader()
+  fadeArrow()
+  checkBlobs()
+})
+
